d09bf72c0d8754893f41d313c402fc5551d44ee1
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-06-06 10:31:59 +0200

    v1 app

--- TODO.md
+++ TODO.md
@@ -1,10 +1,13 @@
 # TODO
 
-- [ ] bmi between 18.5 and 25 : green - normal
-- [ ] bmi below 18.5 : pink - too skinny
-- [ ] bmi between 25 and 30 : yellow - overweight
-- [ ] bmi between 30 and 40 : orange - obesity
-- [ ] bmi over 40 : red - morbid obesity
+- [x] bmi between 18.5 and 25 : green - normal
+- [x] bmi below 18.5 : pink - too skinny
+- [x] bmi between 25 and 30 : yellow - overweight
+- [x] bmi between 30 and 40 : orange - obesity
+- [x] bmi over 40 : red - morbid obesity
 - [ ] bmi tracker: date, value, graph
 - [ ] profile: save age, height, genre, local storage
-- [ ] bmi on second screen
+- [x] bmi on second screen
+- [x] feedback on genre select
+
+* [doc](https://www.santepratique.fr/nutrition/calcul-imc)
--- src/components/BMICalculator.tsx
+++ src/components/BMICalculator.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useRef, useEffect } from "react";
+import FadeIn from "react-fade-in";
 
 /** male =1, female = 0 */
 const GENRE_DEFAULT = 0;
@@ -6,14 +7,33 @@ const HEIGHT_DEFAULT = 165;
 const WEIGHT_DEFAULT = 60;
 const AGE_DEFAULT = 18;
 
+const SKINNY = "🤔   Too skinny";
+const NORMAL = "✔ Normal";
+const OVERWEIGHT = "🛑 Overweight";
+const OBESITY = "🤕 Obesity";
+const MORBID_OBESITY = "🤒 Morbid Obesity";
+
 const BMICalculator: FC = () => {
   const [genre, setGenre] = useState(GENRE_DEFAULT);
   const [height, setHeight] = useState(HEIGHT_DEFAULT);
   const [weight, setWeight] = useState(WEIGHT_DEFAULT);
   const [age, setAge] = useState(AGE_DEFAULT);
+  const [showResults, setShowResults] = useState(false);
 
-  const handleGenreChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+  const maleClassName = useRef("male");
+  const femaleClassName = useRef("female active");
+  const [bmiClassName, setBmiClassName] = useState("bmi");
+
+  const handleGenreChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (genre === 0) {
+      maleClassName.current = "male active";
+      femaleClassName.current = "female";
+    } else {
+      maleClassName.current = "male ";
+      femaleClassName.current = "female active";
+    }
     setGenre(parseInt(e.target.value));
+  };
 
   const handleHeightChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setHeight(parseInt(e.target.value));
@@ -34,85 +54,113 @@ const BMICalculator: FC = () => {
   const bfi = (1.2 * Number(bmi) + 0.23 * age - 10.8 * genre - 5.4).toFixed(2);
 
   /** Save BMI and BFI values and the date */
-  const save = (): void => {
-    localStorage.setItem(Date.now().toString(), bmi);
-    localStorage.setItem(Date.now().toString(), bfi);
-  };
+  const save = (): void => setShowResults(!showResults);
+
+  useEffect(() => {
+    if (Number(bmi) < 18.5) {
+      setBmiClassName(SKINNY);
+    } else if (Number(bmi) < 25) {
+      setBmiClassName(NORMAL);
+    } else if (Number(bmi) < 30) {
+      setBmiClassName(OVERWEIGHT);
+    } else if (Number(bmi) < 40) {
+      setBmiClassName(OBESITY);
+    } else {
+      setBmiClassName(MORBID_OBESITY);
+    }
+  }, [bmi]);
 
   return (
     <div className="calculator">
-      <h1>BMI Calculator</h1>
-      <div className="genre">
-        <label htmlFor="male">
-          <i className="fa fa-4x fa-mars"></i>
-        </label>
-        <input
-          type="radio"
-          name="genre"
-          id="male"
-          value={1}
-          onChange={handleGenreChange}
-          checked={genre === 1}
-        />
-        <label htmlFor="female">
-          <i className="fa fa-4x fa-venus"></i>
-        </label>
-        <input
-          type="radio"
-          name="genre"
-          id="female"
-          value={0}
-          onChange={handleGenreChange}
-          checked={genre === 0}
-        />
-      </div>
-      <div className="height">
-        <label htmlFor="height">Height</label>
-        <p>{height} cm</p>
-        <input
-          type="range"
-          name="height"
-          id="height"
-          min={100}
-          max={200}
-          value={height}
-          onChange={handleHeightChange}
-        />
-      </div>
-      <div className="numeric-inputs">
-        <div className="input-group">
-          <label htmlFor="weight">Weight</label>
+      <FadeIn delay={100}>
+        {/* <h1>BMI Calculator</h1> */}
+        <div className="genre">
+          <div className={maleClassName.current}>
+            <label htmlFor="male">
+              <i className="fa fa-4x fa-mars"></i>
+              <input
+                type="radio"
+                name="genre"
+                id="male"
+                value={1}
+                onChange={handleGenreChange}
+                checked={genre === 1}
+              />
+            </label>
+          </div>
+          <div className={femaleClassName.current}>
+            <label htmlFor="female">
+              <i className="fa fa-4x fa-venus"></i>
+            </label>
+            <input
+              type="radio"
+              name="genre"
+              id="female"
+              value={0}
+              onChange={handleGenreChange}
+              checked={genre === 0}
+            />
+          </div>
+        </div>
+        <div className="height">
+          <label htmlFor="height">Height</label>
+          <p>{height} cm</p>
           <input
-            type="number"
-            name="weight"
-            id="weight"
-            min={1}
-            value={weight}
-            onChange={handleWeightChange}
+            type="range"
+            name="height"
+            id="height"
+            min={100}
+            max={200}
+            value={height}
+            onChange={handleHeightChange}
           />
         </div>
-        <div className="input-group">
-          <label htmlFor="weight">Age</label>
-          <input
-            type="number"
-            name="age"
-            id="age"
-            min={0}
-            max={100}
-            value={age}
-            onChange={handleAgeChange}
-          />
+        <div className="numeric-inputs">
+          <div className="input-group">
+            <label htmlFor="weight">Weight (kg)</label>
+            <input
+              type="number"
+              name="weight"
+              id="weight"
+              min={1}
+              value={weight}
+              onChange={handleWeightChange}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="weight">Age</label>
+            <input
+              type="number"
+              name="age"
+              id="age"
+              min={0}
+              max={100}
+              value={age}
+              onChange={handleAgeChange}
+            />
+          </div>
         </div>
-      </div>
 
-      <div>
-        <h2>Your BMI is</h2>
-        <p>{bmi}</p>
-        <h2>Your BFI is</h2>
-        <p>{bfi} %</p>
-      </div>
+        {/* {showResults && ( */}
+        <FadeIn>
+          <div className="results">
+            <div className="bmi">
+              <h2>Your BMI is</h2>
+              <p>{bmi}</p>
+            </div>
+            <div className="bfi">
+              <h2>Your BFI is</h2>
+              <p>{bfi} %</p>
+            </div>
+          </div>
+          <div className="description">
+            <h3>{bmiClassName}</h3>
+          </div>
+        </FadeIn>
+        {/* )} */}
 
-      <button onClick={save}>Save</button>
+        <button onClick={save}>Save Results</button>
+      </FadeIn>
     </div>
   );
 };
